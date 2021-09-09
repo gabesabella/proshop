fb2108f1791b71f330e86d5c678a5108cf1acc0e
gabereal373 <gabereal373@gmail.com>
2021-09-09 17:18:49 -0400

    cartscreen fix

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -12,11 +12,11 @@ const CartScreen = ({ match, location, history }) => {
 
   const dispatch = useDispatch()
 
-  const cart = useSelector(state => state.cart) 
+  const cart = useSelector((state) => state.cart)
   const { cartItems } = cart
 
   useEffect(() => {
-    if(productId) { 
+    if (productId) {
       dispatch(addToCart(productId, qty))
     }
   }, [dispatch, productId, qty])
@@ -29,27 +29,28 @@ const CartScreen = ({ match, location, history }) => {
     history.push('/login?redirect=shipping')
   }
 
-  return <Row>
-    <Col md={8}>
-      <h1>Shopping Cart</h1>
-      {cartItems.length === 0 ? (
-        <Message>
-          Your cart is empty <Link to='/'></Link> 
-        </Message>
-      ) : (
-        <ListGroup variant='flush'>
-          {cartItems.map(item => (
-            <ListGroup.Item key='{item.product}'>
-              <Row>
-                <Col md={2}>
-                  <Image src={item.image} alt={item.name} fluid rounded />
-                </Col>
-                <Col md={3}>
-                  <Link to={`/product${item.product}`}>{item.name}</Link>
-                </Col>
-                <Col md={2}>${item.price}</Col>
-                <Col md={2}>
-                <Form.Control
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to='/'>Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
                       as='select'
                       value={item.qty}
                       onChange={(e) =>
@@ -64,35 +65,50 @@ const CartScreen = ({ match, location, history }) => {
                         </option>
                       ))}
                     </Form.Control>
-                </Col>
-                <Col md={2}>
-                  <Button type='button' variant='light' onClick={() => removeFromCartHandler(item.product)}>
-                    <i className='fas fa-trash'></i>
-                  </Button>
-                </Col>
-              </Row>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type='button'
+                      variant='light'
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className='fas fa-trash'></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items
+              </h2>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
             </ListGroup.Item>
-          ))}
-        </ListGroup>
-      )}
-    </Col>
-    <Col md={4}>
-       <Card>
-        <ListGroup variant='flush'>
-          <ListGroup.Item>
-            <h2>Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})</h2>
-            ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <Button type='button' className='col-12' disabled={cartItems.length === 0} onClick={checkoutHandler}>
-              Proceed To Checkout            
-            </Button>
-          </ListGroup.Item>
-        </ListGroup>
-      </Card>                 
-    </Col>
-  </Row>
-  
+            <ListGroup.Item>
+              <Button
+                type='button'
+                className='btn-block'
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
+              >
+                Proceed To Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  )
 }
 
 export default CartScreen
