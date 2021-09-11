b9362fdc07097af61abc40683b69726513dd7bdc
gabereal373 <gabereal373@gmail.com>
2021-09-11 17:59:04 -0400

    dank styles

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Row, Col, Container } from 'react-bootstrap'
 
 const SearchBox = ({ history }) => {
   const [keyword, setKeyword] = useState('')
@@ -15,16 +15,25 @@ const SearchBox = ({ history }) => {
 
   return (
     <Form onSubmit={submitHandler} inline>
-      <Form.Control
+      
+    <Container>
+      <Row>
+        <Col id='searchBar'>
+        <Form.Control
         type='text'
         name='q'
         onChange={(e) => setKeyword(e.target.value)}
         placeholder='Search Products...'
         className='mr-sm-2 ml-sm-5'
       ></Form.Control>
-      <Button type='submit' variant='outline-success' className='p-2 search-button'>
+        </Col>
+        <Col>
+        <Button type='submit' variant='outline-success' id='searchButton' className='p-1.5'>
         Search
-      </Button>
+      </Button> 
+        </Col>
+     </Row>
+    </Container>
     </Form>
   )
 }
