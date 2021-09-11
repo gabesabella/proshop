aaf81bc6af8742f04c6f5b2328e4231c7756357a
gabereal373 <gabereal373@gmail.com>
2021-09-11 18:23:32 -0400

    style changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -21,12 +21,12 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>ProShop</Navbar.Brand>
+            <Navbar.Brand>Gabe's ProShop</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Route render={({ history }) => <SearchBox history={history} />} />
-            <Nav className='ml-auto'>
+            <Nav className='ms-auto three-links'>
               <LinkContainer to='/cart'>
                 <Nav.Link>
                   <i className='fas fa-shopping-cart'></i> Cart
