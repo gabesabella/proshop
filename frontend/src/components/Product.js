df4c3b638931bf7ae3283cb3660e29d237a28b5a
gabereal373 <gabereal373@gmail.com>
2022-04-07 13:28:34 -0400

    pagination fixed, added images

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -7,7 +7,7 @@ const Product = ({ product }) => {
   return (
     <Card className='product-card my-3 p-3 rounded'>
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' />
+        <Card.Img classNamme='product-img' src={product.image} variant='top' />
       </Link>
 
       <Card.Body>
